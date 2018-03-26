0e08f650ef78ee722d73084bbf7b46d31cd9b327
Roee <roeeshachar@gmail.com>
2018-03-26 23:27:35 +0300

    intiail

---
+++ src/main/java/WebConfig.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("main.java")
+public class WebConfig {
+
+    // ...
+}
